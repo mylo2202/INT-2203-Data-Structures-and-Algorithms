b524381d63ce5033dc6c72b48fe6d1936088db16
mylo2202 <48115847+mylo2202@users.noreply.github.com>
2019-09-19 12:14:38 +0700

    Initial commit

---
+++ 1.1 hellofuckingworld/src/UseThree.java
@@ -0,0 +1,13 @@
+public class UseThree
+{
+    public static void main(String[] args)
+    {
+        System.out.print("greetings, ");
+        System.out.print(args[3]);
+        System.out.print(", ");
+        System.out.print(args[1]);
+        System.out.print(", and ");
+        System.out.print(args[2]);
+        System.out.println(". fuck off all of you.");
+    }
+}
